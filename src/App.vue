7fda17fac748841f6bbed18f4ce0359724e04ed5
teriTheDesigner <tere1109@stud.kea.dk>
2023-08-08 20:16:55 +0200

    fetching data

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="pokemon in pokemonData" :key="pokemon.name">
+          <h2>{{ pokemon.name }}</h2>
+          <img :src="pokemon.imageUrl" :alt="pokemon.name" />
+          <p>Weight: {{ pokemon.desc }}</p>
+          <p>Base Experience: {{ pokemon.exp }}</p>
+          <p>Types: {{ pokemon.types.join(', ') }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted } from 'vue';
+  
+  export default {
+    setup() {
+      const pokemonData = ref([]);
+  
+      onMounted(async () => {
+        try {
+          const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30");
+          const data = await res.json();
+  
+          const fetchedPokemonData = await Promise.all(
+            data.results.map(async (pokemon) => {
+              const res = await fetch(pokemon.url);
+              const pokemonData = await res.json();
+              const typeNames = pokemonData.types.map((type) => type.type.name);
+  
+              return {
+                pokemonData: pokemonData,
+                name: pokemon.name,
+                imageUrl: pokemonData.sprites.other["official-artwork"].front_default,
+                desc: pokemonData.weight,
+                exp: pokemonData.base_experience,
+                types: typeNames,
+              };
+            })
+          );
+  
+          pokemonData.value = fetchedPokemonData;
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      });
+  
+      return {
+        pokemonData,
+      };
+    },
+  };
+  </script>
+  
+  <style>
+body{
+  background-color: black;
+  color: white;
+  margin: 0;
+  
+}
+ul{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+img{
+  width: 100%;
+}
+
+div{
+  margin: 2rem;
+}
+  </style>
+  
